76ec22558fa7ac9c0e2dc273c1df80e7d2701f32
Jean-Yves Le Cabellec <jy.lecabellec@gmail.com>
2018-05-24 09:38:20 +0200

    Populating User Information
    Add error message getting information on profile taht doesn't exist

--- Simple Dynamic Site/router.js
+++ Simple Dynamic Site/router.js
@@ -44,6 +44,7 @@ function user(request, response) {
         // on "error"
         studentProfile.on("error",function(error){
             // show the error html
+            response.write(error.message + '\n');
             response.end('Footer\n');
         });
 
